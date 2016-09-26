aad193ec1be6e077dbce928ce8d93e61cf83a222
geektech893916 <geektech893916@gmail.com>
2016-09-26 13:10:52 +0000

    Add show and edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,30 @@ class ArticlesController < ApplicationController
 
     # save the article then redirect to show template
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -18,3 +40,10 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
   end
 end
+
+
+
+
+
+
+
